b7acc72072cd0dbe8eae604071808a1b9d56bfeb
tgramvn <109977670+tgramvn@users.noreply.github.com>
2022-12-06 22:16:34 +0700

    Add files via upload

---
+++ tt/a.js
@@ -0,0 +1,16 @@
+﻿var cron = require('node-cron');
+const request = require('request');
+
+// THAY SỐ 2 THÀNH SỐ GIÂY CẦN CRON
+
+/** 
+ * CRON MINIGAME - SỐ GIÂY
+ */
+cron.schedule('*/7 * * * * *', () => {
+    request('https://joker.clmm48.com/xu-ly-minigame', { json: true }, (err, res, body) => {
+        if (err) { return console.log(err); }
+        console.log(body);
+        console.log("da chay cron minigame - cho 7s");
+    });
+});
+
